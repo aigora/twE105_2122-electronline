6833ef5fb6d13954ccdd641e249817be86806fab
Juan <99480934+Juanmdemiguel@users.noreply.github.com>
2022-04-07 19:49:35 +0200

    Cambios en menú
    todavia sujeto a mejora

--- Menú/Menú.c
+++ Menú/Menú.c
@@ -1,62 +1,116 @@
 #include <stdio.h>
 #include <locale.h>
+
 int main()
 {
+	system("color f1");
     setlocale(LC_ALL,"spanish");
-    int op,tam,dificultad,fondo,musica;
+
+    int op,dificultad,fondo, colum, fila;
+    int musica = 0; //música en 0 indica que esta apagada, mientras que uno que está encendida.
+
     printf("************************************************************************\n");
     printf("************************************************************************\n");
     printf("**************¡BIENVENIDO JUGADOR AL LABERINTO ELÉCTRICO!***************\n");
     printf("************  Elige una opción para personalizar el juego. *************\n");
     printf("************************************************************************\n");
     printf("************************************************************************\n");
-    do
-    {
-        printf("\n");
+
+    inicio:
+do{
+    printf("\n");
         printf("-Presiona 1 para empezar el juego.\n");
         printf("\n");
         printf("-Presiona 2 para elegir el tamaño del laberinto.\n");
         printf("\n");
         printf("-Presiona 3 para elegir la dificultad del laberinto.\n");
         printf("\n");
-        printf("-Presiona 4 para elegir un fondo para el laberinto.\n");
+        printf("-Presiona 4 para elegir un fondo para el laberinto y el menú.\n");
         printf("\n");
-        printf("-Presione 5 para activar/desactivar música.\n");
+        printf("-Presiona 5 para activar/desactivar música.\n");
         printf("\n");
-        printf("-Presione cualquier otra tecla para salir del menú.\n");
+        printf("-Presiona 6 para salir del menú.\n");
         printf("\n");
         scanf("%d",&op);
+        system("cls"); //borra toda la información de la ventana
+
+     }while((op < 1) || (op > 6));
+
+
         switch (op)
         {
         case 1:
+            //empieza el juego
             break;
+
         case 2:
-            printf("Presione 1 para un tamaño pequeño.\n");
-            printf("Presione 2 para un tamaño mediano.\n");
-            printf("Presione 3 para un tamaño grande.\n");
-            printf("%d",&tam);
+            printf("Introduzca la altura del laberinto.\n");
+            scanf("%i", &colum);
+            printf("Introduzca la anchura del laberinto.\n");
+           scanf("%i", &fila);
+
+            system("cls");
+            goto inicio;
             break;
+
         case 3:
+            do{
             printf("Presione 1 para dificultad fácil.\n");
             printf("Presione 2 para dificultad media.\n");
             printf("Presione 3 para dificultad difícil.\n");
-            printf("%d",&dificultad);
+            scanf("%d",&dificultad);
+            } while(dificultad < 1 || dificultad > 3);
+            system("cls");
+            goto inicio;
             break;
+
         case 4:
+            do{
             printf("Presione 1 para un fondo negro.\n");
             printf("Presione 2 para un fondo blanco.\n");
             printf("Presione 3 para un fondo rojo.\n");
             printf("Presione 4 para un fondo azul.\n");
             scanf("%d",&fondo);
+            } while(fondo < 1 || fondo > 4);
+
+            switch(fondo){
+        case 1:
+            system("color 0f");
+            break;
+
+        case 2:
+            system("color f0");
+            break;
+
+        case 3:
+            system("color 40");
+            break;
+
+        case 4:
+            system("color b0");
+            break;
+        default:
+            break;
+            }
+             system("cls");
+            goto inicio;
+
             break;
         case 5:
-            printf("Presione 0 para desactivar música.\n");
-            scanf("%d",&musica);
+           if (musica == 0) // si la musica está apagada la enciende
+            musica = 1;
+           else if (musica == 1) // si esta encendida la apaga
+            musica = 0;
+             system("cls");
+            goto inicio;
+
         break;
+         case 6:
+             exit(-1);
+             break;
+
         default:
             break;
         }
-    }
-    while((op<1) || (op>6));
     return 0;
 }
