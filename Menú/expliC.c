a6ccb820770efc50c50f470d4e57249c6c02b9b8
Diego <diego.rgonzalez@alumnos.upm.es>
2022-04-13 15:52:39 +0200

    Error de ficheros arreglado

--- Menú/expliC.c
+++ Menú/expliC.c
@@ -1,12 +1,13 @@
 #include "expliC.h"
 void controles(void){
+printf("\n");
 printf("Presione W para avanzar hacia arriba.\n");
 printf("Presione A para avanzar hacia la izquierda.\n");
 printf("Presione S para avanzar hacia abajo.\n");
 printf("Presione D para avanzar hacia la derecha.\n");
-printf("\n");
 }
 
 void explicacion(void){
+printf("\n");
 printf("Aquí va la explicación\n");
 }
