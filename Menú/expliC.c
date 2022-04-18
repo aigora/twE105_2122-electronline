f63c4a6cb8f431830ad9d1a08151fc89f67b53ba
Diego <diego.rgonzalez@alumnos.upm.es>
2022-04-18 12:48:56 +0200

    Cambios en el menú
    Añado explicación

--- Menú/expliC.c
+++ Menú/expliC.c
@@ -9,5 +9,10 @@ printf("Presione D para avanzar hacia la derecha.\n");
 
 void explicacion(void){
 printf("\n");
-printf("Aquí va la explicación\n");
+printf("Bienvenido al laberinto eléctrico. En este juego se pondrá a prueba tu velocidad para completar varios laberintos, de\n");
+printf("distinta dificultad, para llegar a tener el mayor récord. Este juego incluye varias opciones, como poder utilizar \n");
+printf("objetos(que te pueden beneficiar o perjudicar) repartidos por el mapa, y la posibilidad de elegir entre una amplia\n");
+printf("variedad de laberintos. Además, se incluye la posibilidad de cambiar de avatar y de incluir música.\n");
+printf("Si desea conocer los objetos disponibles, pulse 1.\n");
+printf("Si desea volver al menú, pulse 2.\n");
 }
