65c4ad774c2d49165acad4529f5f52d92d9cb22e
Diego <diego.rgonzalez@alumnos.upm.es>
2022-05-20 14:02:30 +0200

    Elimnación de goto y de system(a medias)

--- Menú(con juego) sin SDL/Menú.c
+++ Menú(con juego) sin SDL/Menú.c
@@ -9,7 +9,7 @@
 void expobj(void);
 int main()
 {
-	system("color f1");
+
     setlocale(LC_ALL,"spanish");
 
     int op,dificultad,fondo, colum=17, fila=17,obj;   //Laberinto 13 x 13 predeterminado
@@ -25,119 +25,110 @@ int main()
     printf("************************************************************************\n");
     printf("************************************************************************\n");
 
-    inicio:
-do{
-    printf("\n");
+
+    do
+    {
+        printf("\n");
         printf("-Presiona 1 para empezar el juego.\n");
         printf("\n");
         printf("-Presiona 2 para elegir el tamaño del laberinto.\n");
         printf("\n");
-        printf("-Presiona 3 para elegir la dificultad del laberinto.\n");
+        printf("-Presiona 3 para elegir un fondo para el laberinto y el menú.\n");
         printf("\n");
-        printf("-Presiona 4 para elegir un fondo para el laberinto y el menú.\n");
+        printf("-Presiona 4 para ver las puntuaciones.\n");
         printf("\n");
-        printf("-Presiona 5 para ver las puntuaciones.\n");
+        printf("-Presiona 5 para acceder a la explicación y los controles.\n");
         printf("\n");
-        printf("-Presiona 6 para activar/desactivar los objetos.\n");
-        printf("\n");
-        printf("-Presiona 7 para acceder a la explicación y los controles.\n");
-        printf("\n");
-        printf("-Presiona 8 para salir del menú.\n");
+        printf("-Presiona 6 para salir del menú.\n");
         printf("\n");
         scanf("%d",&op);
         printf("\x1b[2J"); //borra toda la información de la ventana.
 
-     }while((op < 1) || (op > 8));
+
 
 
         switch (op)
         {
         case 1:
+
             juego_lab(colum,fila);
-            goto inicio;
+
+            printf("\x1b[2J");
             break;
 
+
         case 2:
+
             printf("Introduzca la altura del laberinto. Predeterminado: 17.\n");
             scanf("%i", &colum);
             printf("Introduzca la anchura del laberinto. Predeterminado: 17.\n");
             scanf("%i", &fila);
 
             printf("\x1b[2J");
-            goto inicio;
             break;
+
 
         case 3:
-            do{
-            printf("Presione 1 para dificultad fácil.\n");
-            printf("Presione 2 para dificultad media.\n");
-            printf("Presione 3 para dificultad difícil.\n");
-            scanf("%d",&dificultad);
-            } while(dificultad < 1 || dificultad > 3);
-            printf("\x1b[2J");
-            goto inicio;
-            break;
 
-        case 4:
-            do{
-            printf("Presione 1 para un fondo negro.\n");
-            printf("Presione 2 para un fondo blanco.\n");
-            printf("Presione 3 para un fondo rojo.\n");
-            printf("Presione 4 para un fondo azul.\n");
-            scanf("%d",&fondo);
-            } while(fondo < 1 || fondo > 4);
-
-            switch(fondo){
-        case 1:
-            system("color 0f");
-            break;
-        case 2:
-            system("color f0");
-            break;
-        case 3:
-            system("color 40");
-            break;
-        case 4:
-            system("color b0");
-            break;
-        default:
-            break;
+            do
+            {
+                printf("Presione 1 para un fondo negro.\n");
+                printf("Presione 2 para un fondo blanco.\n");
+                printf("Presione 3 para un fondo rojo.\n");
+                printf("Presione 4 para un fondo azul.\n");
+                scanf("%d",&fondo);
             }
-             printf("\x1b[2J");
-            goto inicio;
+            while(fondo < 1 || fondo > 4);
+
+            switch(fondo)
+            {
+            case 1:
+                system("color 0f");
+                break;
+            case 2:
+                system("color f0");
+                break;
+            case 3:
+                system("color 40");
+                break;
+            case 4:
+                system("color b0");
+                break;
+            default:
+                break;
+            }
+            printf("\x1b[2J");
             break;
-        case 5:
+
+
+        case 4:
 
             // Abrimos fichero para lectura
-        /**
+
             pf = fopen("Puntuacion.txt", "r");
-            if (pf == NULL) {// Si el resultado es NULL mensaje de error
+            if (pf == NULL)
+            {
+                // Si el resultado es NULL mensaje de error
                 printf("Error al abrir el fichero.\n");
-                    return -1;
-                }
+                return -1;
+            }
             else
             {
-            // Leemos datos separados por comas
-            n = fscanf(pf,"[^*****Resultados del intento número 1*****\n] %i [^minuto(s) y ] %i [^segundos -------->] %i",mins,seg,puntuacion);
-            fclose(pf);
-            // Mostramos en pantalla lo leído
-            printf(" %i\n",puntuacion);
-            Sleep(5000);
+                // Leemos los datos
+                n = fscanf(pf,"%d,%d,%d",&mins,&seg,&puntuacion);
+                fclose(pf);
+                // Mostramos en pantalla lo leído
+                printf("***Resultados del intento realizado***\n");
+                printf("%d minuto(s) y %d segundos --------> %d puntos",mins,seg,puntuacion);
+                Sleep(5000);
             }
-            goto inicio;
-            break;
-        */
 
-        case 6:
-            if (objetos==0)
-                objetos=1;
-            else if (objetos==1)
-                objetos=0;
-            printf("\x1b[2J");
-            goto inicio;
             break;
 
-        case 7:
+
+
+        case 5:
+
             printf("Escriba 'controles' para ver los controles del juego o 'explicacion' para la explicación del juego:\n");
             scanf("%20s",opcion);
             if (strcmp(opcion,cont)==0)
@@ -151,24 +142,28 @@ do{
                 expobj();
                 sleep(20);
                 printf("\x1b[2J");
-                goto inicio;
                 break;
             case 2:
                 printf("\x1b[2J");
-                goto inicio;
                 break;
             default:
                 break;
             }
             break;
 
-         case 8:
-             exit(-1);
-             break;
+
+        case 6:
+            exit(-1);
+            break;
 
         default:
             break;
         }
+
+    }
+    while((op >= 1) || (op <= 6));
+
+
     return 0;
 }
 void expobj(void)
