e6e8a34ac9316d116e30e3fc06187821441f5e87
Diego <diego.rgonzalez@alumnos.upm.es>
2022-05-23 12:12:06 +0200

    Todos los tokens en un fichero

--- Laberinto definitivo SDL/Mov2.c
+++ Laberinto definitivo SDL/Mov2.c
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Tokens.h"
 
 void InitGame(int *stage,int pararaudio)
 {
@@ -206,6 +207,7 @@ void InitGame(int *stage,int pararaudio)
                     {
                         teletransportar=1;
                     }
+
                     //Movimiento
                     if (event.type == SDL_KEYDOWN) //Cuando estemos pulsando la tecla
                     {
@@ -603,38 +605,7 @@ for(k=0;k<4;k++)
 
 }
 
-//Función para los objetos
-void InitTokens(int maze[N][N])
-{
-    int i, r1, r2;
-      for (i = 0; i < 2; i ++){
-            do{
-        r1 =  2 + rand() % 19;
-        r2 =  2 + rand() % 19;
-              } while(maze[r1][r2] == 1 || (r1 == 1 && r2 == 1));
-        maze[r1][r1] = 2; //2 para las lunas
-    }
 
-
-    for (i = 0; i < 2; i ++){
-            do{
-        r1 =  2 + rand() % 19;
-        r2 = 2 + rand() % 19;
-              } while(maze[r1][r2] == 1 || maze[r1][r2] == 2 || (r1 == 1 && r2 == 1));
-
-        maze[r1][r2] = 3; //3 para los soles
-    }
-
-
-    for (i = 0; i < 2; i ++){
-            do{
-        r1 =  2 + rand() % 19;
-        r2 = 2 + rand() % 19;
-              }while(maze[r1][r2] == 1 || maze[r1][r2] == 2 || maze[r1][r2] == 3 || (r1 == 1 && r2 == 1));
-        maze[r1][r2] = 4; //4 para los tps
-    }
-
-}
 
 //Función para la salida
 int salir(int coordx,int coordy, Token Final)
@@ -647,19 +618,6 @@ else
 {
     return 0;
 }
-}
-
-void TLuna(int coordx, int coordy, Token Luna[2], int *inversion)
-{
-    if(((Luna[0].x==coordx)&&(Luna[0].y==coordy))||((Luna[1].x==coordx)&&(Luna[1].y==coordy)))
-                    *inversion=1;
-}
-
-void TSol(int coordx, int coordy, Token Sol[2], int *inversion)
-{
-      if(((Sol[0].x==coordx)&&(Sol[0].y==coordy))||((Sol[1].x==coordx)&&(Sol[1].y==coordy)))
-                    *inversion=0;
-
 }
 
 
