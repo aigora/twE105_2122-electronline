0e693a38ba1a3291bf695b080010436d5eb6bb26
Juan <99480934+Juanmdemiguel@users.noreply.github.com>
2022-05-23 18:00:16 +0200

    Cambios menores a generación de tp

--- Laberinto definitivo SDL/Tokens.c
+++ Laberinto definitivo SDL/Tokens.c
@@ -6,8 +6,8 @@ void InitTokens(int maze[N][N])
     int i, r1, r2;
       for (i = 0; i < 2; i ++){
             do{
-        r1 =  2 + rand() % 19;
-        r2 =  2 + rand() % 19;
+        r1 =  1 + rand() % 20; //numeros aleatorios del 1 al 20
+        r2 =  1 + rand() % 20;
               } while(maze[r1][r2] == 1 || (r1 == 1 && r2 == 1));
         maze[r1][r1] = 2; //2 para las lunas
     }
@@ -15,21 +15,26 @@ void InitTokens(int maze[N][N])
 
     for (i = 0; i < 2; i ++){
             do{
-        r1 =  2 + rand() % 19;
-        r2 = 2 + rand() % 19;
+        r1 =  1 + rand() % 20;
+        r2 = 1 + rand() % 20;
               } while(maze[r1][r2] == 1 || maze[r1][r2] == 2 || (r1 == 1 && r2 == 1));
 
         maze[r1][r2] = 3; //3 para los soles
     }
 
-
-    for (i = 0; i < 2; i ++){
+//Cada TP se generará en una zona de la pantalla
             do{
-        r1 =  2 + rand() % 19;
-        r2 = 2 + rand() % 19;
+        r1 =  1 + rand() % 10;
+        r2 = 10 + rand() % 10;
               }while(maze[r1][r2] == 1 || maze[r1][r2] == 2 || maze[r1][r2] == 3 || (r1 == 1 && r2 == 1));
         maze[r1][r2] = 4; //4 para los tps
-    }
+
+       do{
+        r1 =  10 + rand() % 10;
+        r2 = 1 + rand() % 10;
+              }while(maze[r1][r2] == 1 || maze[r1][r2] == 2 || maze[r1][r2] == 3 || (r1 == 1 && r2 == 1));
+        maze[r1][r2] = 4; //4 para los tps
+
 
 }
 
