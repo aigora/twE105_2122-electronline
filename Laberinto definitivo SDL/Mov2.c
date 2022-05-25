d24912c3b76d8c760c7bac01e4ff10b21a93593b
Juan <99480934+Juanmdemiguel@users.noreply.github.com>
2022-05-25 16:49:46 +0200

    añadida interacción con tesoro y logo definitivo

--- Laberinto definitivo SDL/Mov2.c
+++ Laberinto definitivo SDL/Mov2.c
@@ -6,6 +6,11 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
     //Declaración de variables
     const int SCREEN_WIDTH = 1080;
     const int SCREEN_HEIGHT = 720;
+ int i, j;
+    int alt = 21;
+    int anch = 21;
+
+
 
     int maze[N][N];
     Token Teleport[2];
@@ -14,6 +19,11 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
     Token Final;
     Token Idolo;
 
+  for (i = 0; i < alt; i++)
+    {
+        for(j = 0; j < anch; j++)
+            maze[i][j] = 1;
+    }
 
    int xo = 1 + rand()%20;//Coordenadas aleatorias de 1 a 21 de generacion inicial x
    int yo = 1 + rand()%20;//Coordenadas aleatorias de 1 a 21 de generacion inicial y
@@ -38,6 +48,8 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
 
     SDL_Window* ventana = SDL_CreateWindow( "Movimiento", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
     SDL_Surface* screenSurface = NULL;
+      SDL_Surface* screenSurface2 = NULL;
+
     Uint32 render_flags = SDL_RENDERER_ACCELERATED;
     SDL_Renderer* rend = SDL_CreateRenderer(ventana, -1, render_flags);
     SDL_Surface *Pers;
@@ -77,6 +89,10 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
     int coordx=xo,coordy=yo;//Coordenadas del personaje inicializadas en la generacion del laberinto
     int teletransportar=1;//Variable de teletransportar activada
 
+    recursion(xo, yo, anch, alt, maze);
+    InitTokens(maze);
+    maze[xo][yo] = 9;
+
     //Se inicia la música
     SDL_Init(SDL_INIT_AUDIO);//Iniciamos el audio.
     SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
@@ -112,11 +128,13 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
                 return;
             }
 
-    screenSurface = SDL_GetWindowSurface( ventana );
+        screenSurface = SDL_GetWindowSurface( ventana );
+    InitMaze(ventana, screenSurface, rend, maze,coordx,coordy, Luna, Sol, Teleport, &Final, alt, anch, &Idolo);
+        SDL_Texture* texmaze = SDL_CreateTextureFromSurface(rend, screenSurface);
 
-    InitMaze(ventana, screenSurface, rend, maze,coordx,coordy, Luna, Sol, Teleport, &Final, xo, yo, &Idolo);
-    SDL_Texture* texmaze = SDL_CreateTextureFromSurface(rend, screenSurface);
-
+        screenSurface2 = SDL_GetWindowSurface( ventana );
+  InitMaze2(ventana, screenSurface2, rend, maze,coordx,coordy, Luna, Sol, Teleport, &Final, alt, anch, &Idolo);
+        SDL_Texture* texmaze2 = SDL_CreateTextureFromSurface(rend, screenSurface2);
 
 
 
@@ -366,13 +384,19 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
                 SDL_Rect srcrect = {sprite*32, 0, 32, 32};
 
                 //Laberinto pot pantalla
-                if(!quit)
-                {
-                    SDL_RenderClear(rend);
+
+                if (robar == 0){
+                     SDL_RenderClear(rend);
                     SDL_RenderCopy(rend,texmaze, NULL, &dest);
                     SDL_RenderPresent(rend);
-                }
 
+                }
+                    if (robar == 1){
+                     SDL_RenderClear(rend);
+                    SDL_RenderCopy(rend,texmaze2, NULL, &dest);
+                    SDL_RenderPresent(rend);
+
+                    }
 
                 SDL_RenderCopy(rend, texid, &srcrect, &personaje);
                 SDL_RenderPresent(rend);
@@ -420,7 +444,7 @@ void InitGame(int *stage,int pararaudio)//Devuelve el stage del menu y la boolea
 
 
 
-void InitMaze(SDL_Window* window, SDL_Surface* screenSurface, SDL_Renderer* rend, int maze[N][N],int coordx,int coordy,Token Luna[2], Token Sol[2], Token Teleport[2], Token *Final, int xo, int yo, Token *Idolo)
+void InitMaze(SDL_Window* window, SDL_Surface* screenSurface, SDL_Renderer* rend, int maze[N][N],int coordx,int coordy,Token Luna[2], Token Sol[2], Token Teleport[2], Token *Final, int alt, int anch, Token *Idolo)
 {
 
     int t = 0;
@@ -428,8 +452,7 @@ void InitMaze(SDL_Window* window, SDL_Surface* screenSurface, SDL_Renderer* rend
       int s = 0;
 
     int finx, finy;
-    int alt = 21;
-    int anch = 21;
+
     int i,j;
     int randw;
     int srandx, srandy, numwalls = 0;
@@ -437,16 +460,9 @@ void InitMaze(SDL_Window* window, SDL_Surface* screenSurface, SDL_Renderer* rend
     srand(time(NULL));
 
     //Llenamos una matriz de 1. Esta matriz se utilizará posteriormente para hacer el laberinto
-    for (i = 0; i < alt; i++)
-    {
-        for(j = 0; j < anch; j++)
-            maze[i][j] = 1;
-    }
 
 //Hacemos el laberinto
-    recursion(xo, yo, anch, alt, maze);
-    InitTokens(maze);
-    maze[xo][yo] = 9;
+
 
     SDL_Surface *camino = SDL_LoadBMP("Path.bmp"); //Cargamos tanto el sprite del camino como el del muro.
 
@@ -684,4 +700,160 @@ int puntuaciones(int minutos, int segundos)
         else
                 puntuacion=0;
     return puntuacion;
+}
+
+
+void InitMaze2(SDL_Window* window, SDL_Surface* screenSurface, SDL_Renderer* rend, int maze[N][N],int coordx,int coordy,Token Luna[2], Token Sol[2], Token Teleport[2], Token *Final, int alt, int anch, Token *Idolo)
+{
+
+    int t = 0;
+     int l = 0;
+      int s = 0;
+
+    int finx, finy;
+
+    int i,j;
+    int randw;
+    int srandx, srandy, numwalls = 0;
+
+    srand(time(NULL));
+
+    //Llenamos una matriz de 1. Esta matriz se utilizará posteriormente para hacer el laberinto
+
+//Hacemos el laberinto
+
+
+    SDL_Surface *camino = SDL_LoadBMP("Path.bmp"); //Cargamos tanto el sprite del camino como el del muro.
+
+    SDL_Surface *muroG = SDL_LoadBMP("GenWall.bmp");
+    SDL_Surface *muroB = SDL_LoadBMP("BasicWall.bmp");
+    SDL_Surface *muroD = SDL_LoadBMP("DogWall.bmp");
+    SDL_Surface *muroX = SDL_LoadBMP("XatuWall.bmp");
+    SDL_Surface *muroF = SDL_LoadBMP("FWall.bmp");
+
+    SDL_Surface *Inicio = SDL_LoadBMP("Inicio.bmp");
+    SDL_Surface *Salida = SDL_LoadBMP("End.bmp");
+
+    SDL_Surface *Idol = SDL_LoadBMP("Stolen.bmp");
+
+    SDL_Surface *Token1 = SDL_LoadBMP("TK3.bmp"); //luna
+    SDL_Surface *Token3 = SDL_LoadBMP("TK1.bmp"); //sol
+    SDL_Surface *Teleportador1 = SDL_LoadBMP("TP.bmp");
+
+    SDL_Rect pos;
+
+
+
+    //Recorremos la matriz. Cada vez que encuentra un 1 pomdrá un sprite muro, y en un 0 un sprite camino
+    for(i = 0; i < alt; i++)
+    {
+        for(j = 0; j < anch; j++)
+        {
+            if(maze[i][j]==0)
+            {
+                pos.x = 32*i; //Cada vez que tengamos que poner una imagen, tenemos que ponerlo en la posición adecuada
+                pos.y = 32*j; //Como cada imagen es 32x32, al multiplicar la posición en la matriz por su tamaño te la posiciona en la ventana
+                SDL_BlitSurface(camino,0,screenSurface,&pos);
+            }
+            if(maze[i][j]==1)
+            {
+              pos.x = 32*i;
+              pos.y = 32*j;
+
+                  randw= 1 + rand()%10;
+             switch(randw){
+         case 1:
+         case 2:
+         case 3:
+         case 4:
+              SDL_BlitSurface(muroB,0,screenSurface,&pos);
+            break;
+         case 5:
+         case 6:
+         case 7:
+             SDL_BlitSurface(muroG,0,screenSurface,&pos);
+            break;
+         case 8:
+             SDL_BlitSurface(muroF,0,screenSurface,&pos);
+             break;
+         case 9:
+             SDL_BlitSurface(muroX,0,screenSurface,&pos);
+             break;
+         case 10:
+               SDL_BlitSurface(muroD,0,screenSurface,&pos);
+             break;
+             }
+
+            }
+            if(maze[i][j]==2)
+            {
+                pos.x = 32*i;
+                pos.y = 32*j;
+                Luna[l].x = i;
+                Luna[l].y = j;
+
+                SDL_BlitSurface(Token1,0,screenSurface,&pos);
+                l++;
+            }
+             if(maze[i][j]==3)
+            {
+                pos.x = 32*i;
+                pos.y = 32*j;
+                Sol[s].x = i;
+                Sol[s].y = j;
+                SDL_BlitSurface(Token3,0,screenSurface,&pos);
+                s++;
+            }
+
+            if(maze[i][j]==4)
+            {
+                pos.x = 32*i;
+                pos.y = 32*j;
+                Teleport[t].x = i;
+                Teleport[t].y = j;
+                SDL_BlitSurface(Teleportador1,0,screenSurface,&pos);
+                t++;
+            }
+                if(maze[i][j]==5)
+            {
+                pos.x = 32*i;
+                pos.y = 32*j;
+                Idolo->x = i;
+                Idolo->y = j;
+                SDL_BlitSurface(Idol,0,screenSurface,&pos);
+            }
+            if (maze[i][j] == 9)
+            {
+                pos.x = 32*i;
+                pos.y = 32*j;
+                SDL_BlitSurface(Inicio,0,screenSurface,&pos);
+            }
+        }
+    }
+
+
+
+
+                do{
+                srandx = 1 + rand()%20;
+                srandy = 1 + rand()%20;
+                    numwalls = 0;
+                if(maze[srandx - 1][srandy] == 1)
+                    numwalls ++;
+                      if(maze[srandx + 1][srandy] == 1)
+                    numwalls ++;
+                            if(maze[srandx][srandy - 1] == 1)
+                        numwalls ++;
+                            if(maze[srandx][srandy + 1] == 1)
+                        numwalls ++;
+                } while (numwalls != 3 || maze[srandx][srandy] == 1);
+
+
+//printf("%i %i", srandx, srandy);
+
+                            Final->x = srandx;
+                            Final->y = srandy;
+                pos.x = 32*Final->x;
+                pos.y = 32*Final->y;
+                SDL_BlitSurface(Salida,0,screenSurface,&pos);
 }
